9afc5330c53c437b435f7650299831414fe7f749
Rahul Shukla <rahul.shukla13@gmail.com>
2020-09-03 18:35:40 +0530

    Issue #SB-19722 feat: User with admin role should be able to manage ETBs of the tenant

--- src/middlewares/request.middleware.js
+++ src/middlewares/request.middleware.js
@@ -215,6 +215,7 @@ function validateUserToken (req, res, next) {
 function apiAccessForCreatorUser (req, response, next) {
   logger.debug({ msg: 'apiAccessForCreatorUser() called' }, req)
   var userId = req.get('x-authenticated-userid')
+  var isRootOrgAdmin = lodash.has(req.body.request, isRootOrgAdmin) ? req.body.request.isRootOrgAdmin : false
   var data = {}
   var rspObj = req.rspObj
   var qs = {
@@ -253,7 +254,7 @@ function apiAccessForCreatorUser (req, response, next) {
     },
     function (res) {
       let createdBy = res.result.content.createdBy.split(':')
-      if (createdBy[createdBy.length - 1] !== userId && !lodash.includes(res.result.content.collaborators, userId)) {
+      if (createdBy[createdBy.length - 1] !== userId && !lodash.includes(res.result.content.collaborators, userId) && !isRootOrgAdmin) {
         rspObj.errCode = reqMsg.TOKEN.INVALID_CODE
         rspObj.errMsg = reqMsg.TOKEN.INVALID_MESSAGE
         rspObj.responseCode = responseCode.UNAUTHORIZED_ACCESS
